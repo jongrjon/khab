f21fc37e23930a7e1b685a6b771d371b99a633a9
Jón Helgi <jongrjon@gmail.com>
2022-03-08 15:38:38 +0000

    New Product Modal

--- static/js/boutique.js
+++ static/js/boutique.js
@@ -1,8 +1,5 @@
-$(document).on("click", ".product", function (e) {
+function newProduct(e) {
 	e.preventDefault();
-	var $popup = $("#popup");
-	var popup_url = $(this).data("popup-url");
-	$(".modal-body", $popup).load(popup_url, function () {
-		$popup.modal("show");
-	});
-});
+	var $popup = $("#productpopup");
+	$popup.modal("show"); 
+};
